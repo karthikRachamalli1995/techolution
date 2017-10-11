093e0930bf0346db7434abcea032cedf9dd2f102
karthikRachamalli1995 <32642673+karthikRachamalli1995@users.noreply.github.com>
2017-10-11 22:10:39 +0530

    ConsecutiveSum

--- ConsecutiveSum.java
+++ ConsecutiveSum.java
@@ -5,13 +5,11 @@ public class ConsecutiveSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long number = scanner.nextLong();
-        System.out.print(numberOfWays(number));
+        System.out.print(consecutive(number));
     }
 
-    private static int numberOfWays(long number) {
+    static int consecutive(long number) {
         int numberOfPossibleWays = 0;
-        long temp, i, j;
-
         /*
             If the number is 1 then there will one solution that is 0+1
          */
@@ -20,33 +18,32 @@ public class ConsecutiveSum {
         }
 
         /*
-            Iterate upto n/2, after that if we add consecutive numbers then the sum will be
+            Iterate upto n+1/2, after that if we add consecutive numbers then the sum will be
             greater than given number.
             n/2 + (n/2 + 1) is greater than n
         */
-        int start = 0;
-        long end = number / 2;
-        for (i = start; i < end; i++) {
-            /*
-                Store number into a temp variable, By doing this there will be no
-                effect on number of iterations from 0 to n/2
-            */
-            temp = number;
-            j = i;
-            /*
-                Iterate till j is less than temp as we are incrementing the j and subtracting
-                from temp i.e., number
-             */
-            while (j < temp) {
-                temp = temp - j;
-                j++;
-            }
-            /*
-                If J equal to temp that means a possible sum of consecutive numbers are there
-                that there sum results in given number
-             */
-            if (j == temp) {
-                numberOfPossibleWays++;
+
+        long endNumber = (number + 1) / 2;
+        for (long start = 1; start < endNumber; start++) {
+
+            long sumOfConsecutiveNumbers = 0;
+            for (long i = start; i <= endNumber; i++) {
+
+                sumOfConsecutiveNumbers += i;
+
+                /*
+                 if sumOfConsecutiveNumbers is equal to number that means a consecutive sequence update number of possible ways
+                 */
+                if (sumOfConsecutiveNumbers == number) {
+                    numberOfPossibleWays++;
+                    break;
+                }
+
+                /*
+                 if sumOfConsecutiveNumbers is greater than number that means we can't get the consecutive sequence
+                 */
+                if (sumOfConsecutiveNumbers > number)
+                    break;
             }
         }
         return numberOfPossibleWays;
